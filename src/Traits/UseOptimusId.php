2ade9daa84d24a0056a2e27bf902a945211f69f3
龙权 <495502547@qq.com>
2022-09-13 21:02:19 +0800

    Add model trait

---
+++ src/Traits/UseOptimusId.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AntCool\OptimusFactory\Traits;
+
+use AntCool\OptimusFactory\Factory;
+use Illuminate\Database\Eloquent\Model;
+
+trait UseOptimusId
+{
+    public static function bootUseOptimusId(): void
+    {
+        static::created(function (Model $model): void {
+            if (!$model->getAttribute($model->getAttribute($model->getEncodeKey()))) {
+                $model->{$model->getEncodeKey()} = app(Factory::class)->encode($model->getKey(), $model->getEncodeScene());
+                $model->save(['timestamps' => false]);
+            }
+        });
+    }
+
+    public function getEncodeKey(): string
+    {
+        return $this->encodeKey ?? 'encode_id';
+    }
+
+    public function getEncodeScene(): string
+    {
+        return $this->encodeScene ?? 'default';
+    }
+}
